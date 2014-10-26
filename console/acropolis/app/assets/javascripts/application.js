a2f9ec2f9bee3f00cfa1f61a4991df8c750dd01c
babaru <babaru.trinit@gmail.com>
2014-10-26 19:09:06 +0800

    init commit

---
+++ console/acropolis/app/assets/javascripts/application.js
@@ -0,0 +1,25 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery-ui/button
+//= require jquery-ui/datepicker
+//= require jquery-ui/slider
+//= require jquery-ui/spinner
+//= require jquery-ui/tooltip
+//= require jquery-ui/effect
+//= require flatuipro
+//= require twitter/bootstrap
+//= require jquery_ujs
+//= require turbolinks
+//= require retina.min
+//= require_tree .
