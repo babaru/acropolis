b7b3ebadfb287eb050dd53adae69685ffdd1bda1
babaru <babaru.trinit@gmail.com>
2014-10-27 13:47:12 +0800

    add validations of threshold

--- console/acropolis/app/models/risk_plan.rb
+++ console/acropolis/app/models/risk_plan.rb
@@ -3,6 +3,7 @@ class RiskPlan < ActiveRecord::Base
   belongs_to :operation
   belongs_to :user
   belongs_to :product
+  has_many :thresholds
 
   validates :parameter_id, :presence => true
 end
--- console/acropolis/app/models/threshold.rb
+++ console/acropolis/app/models/threshold.rb
@@ -1,4 +1,7 @@
 class Threshold < ActiveRecord::Base
   belongs_to :risk_plan
   belongs_to :relation_symbol
+
+  validates :risk_plan_id, presence: true
+  validates :relation_symbol_id, presence: true
 end
