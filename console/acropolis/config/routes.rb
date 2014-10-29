7a7349e6534cb9fb096b61ab922d5972c9c51f1e
babaru <babaru.trinit@gmail.com>
2014-10-29 23:12:16 +0800

    change layout to make product be central

--- console/acropolis/config/routes.rb
+++ console/acropolis/config/routes.rb
@@ -7,13 +7,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'dashboard#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  resources :products
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :risk_plans do
     get 'delete'
     post 'enable'
