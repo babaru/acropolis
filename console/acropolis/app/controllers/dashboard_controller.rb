144c242b38c4fd13e830b9df84fd1ce598429007
babaru <babaru.trinit@gmail.com>
2014-11-03 22:19:15 +0800

    add recent item panels

--- console/acropolis/app/controllers/dashboard_controller.rb
+++ console/acropolis/app/controllers/dashboard_controller.rb
@@ -6,5 +6,11 @@ class DashboardController < ApplicationController
     @products_monitoring_grid = initialize_grid(Product.all)
     @recent_risk_plans_grid = initialize_grid(RiskPlan.where("id in (#{recent_items(:risk_plan).map {|id, name| [id]}.join(',')})"))
 
+    if recent_items(:product).length > 0
+      @recent_products_grid = initialize_grid(Product.where("id in (#{recent_items(:product).map {|id, name| [id]}.join(',')})"))
+    else
+      @recent_products_grid = initialize_grid(Product.where(id: 0))
+    end
+
   end
 end
