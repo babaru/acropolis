bbd9fd5b474508b6095145ed79b78f2a9fb42ee5
babaru <babaru.trinit@gmail.com>
2014-11-05 23:52:59 +0800

    fix some issue of dashboard grid

--- console/acropolis/app/helpers/dashboard_helper.rb
+++ console/acropolis/app/helpers/dashboard_helper.rb
@@ -2,7 +2,7 @@ module DashboardHelper
   def render_product_parameter(product, parameter)
     p = product.product_risk_parameters.where(parameter_id: parameter.id).first
     if p.nil?
-      ''
+      'N/A'
     else
       if parameter.name == 'in_bound_net_worth' || parameter.name == 'out_bound_net_worth'
         if p.value > 1
