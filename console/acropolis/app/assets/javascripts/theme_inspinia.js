994954ff559f689ae35b6993449c54cd33488b7d
babaru <babaru.trinit@gmail.com>
2014-11-08 23:23:35 +0800

    make ibox work

--- console/acropolis/app/assets/javascripts/theme_inspinia.js
+++ console/acropolis/app/assets/javascripts/theme_inspinia.js
@@ -2,35 +2,28 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 
+//= require jquery-ui
+
 //= require twitter/bootstrap
 //= require wice_grid
 
+// Plugins used by INSPINIA Theme
+
 //= require inspinia/plugins/metisMenu/jquery.metisMenu
-
 //= require inspinia/plugins/slimscroll/jquery.slimscroll.min
-
 //= require inspinia/plugins/flot/jquery.flot
 //= require inspinia/plugins/flot/jquery.flot.tooltip.min
 //= require inspinia/plugins/flot/jquery.flot.spline
 //= require inspinia/plugins/flot/jquery.flot.resize
 //= require inspinia/plugins/flot/jquery.flot.pie
-
 //= require inspinia/plugins/peity/jquery.peity.min
-
-//= require inspinia/inspinia
-
-//= require jquery-ui
-
+//= require inspinia/plugins/switchery/switchery
 //= require inspinia/plugins/gritter/jquery.gritter.min
-
 //= require inspinia/plugins/easypiechart/jquery.easypiechart
-
 //= require inspinia/plugins/sparkline/jquery.sparkline.min
-
 //= require inspinia/plugins/chartJs/Chart.min
 
-//= require inspinia/demo/peity-demo
-//= require inspinia/demo/sparkline-demo
+//= require inspinia/inspinia
 
 //= require nprogress
 //= require nprogress-turbolinks
