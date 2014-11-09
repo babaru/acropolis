64c148bdd70ea8ada0dc31238611dc146fb5111f
babaru <babaru.trinit@gmail.com>
2014-11-09 18:46:28 +0800

    make dashboard and monitoring work

--- console/acropolis/app/assets/javascripts/theme_inspinia.js
+++ console/acropolis/app/assets/javascripts/theme_inspinia.js
@@ -5,13 +5,6 @@
 //= require twitter/bootstrap
 //= require wice_grid
 
-//= require jquery-ui/button
-//= require jquery-ui/datepicker
-//= require jquery-ui/slider
-//= require jquery-ui/spinner
-//= require jquery-ui/tooltip
-//= require jquery-ui/effect
-
 // Plugins used by INSPINIA Theme
 
 //= require inspinia/plugins/metisMenu/jquery.metisMenu
@@ -22,6 +15,9 @@
 //= require inspinia/plugins/flot/jquery.flot.resize
 //= require inspinia/plugins/flot/jquery.flot.pie
 //= require inspinia/plugins/peity/jquery.peity.min
+
+//= require inspinia/inspinia
+
 //= require inspinia/plugins/switchery/switchery
 //= require inspinia/plugins/gritter/jquery.gritter.min
 //= require inspinia/plugins/easypiechart/jquery.easypiechart
@@ -29,7 +25,7 @@
 //= require inspinia/plugins/chartJs/Chart.min
 //= require inspinia/plugins/iCheck/icheck.min
 
-//= require inspinia/inspinia
+//= require jquery-ui
 
 //= require nprogress
 //= require nprogress-turbolinks
