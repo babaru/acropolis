99292477dc3de8d2ac9d521d0269d1a7ad30e08a
babaru <babaru.trinit@gmail.com>
2014-11-13 10:37:05 +0800

    i18n of instrument

--- console/acropolis/app/models/instrument.rb
+++ console/acropolis/app/models/instrument.rb
@@ -1,4 +1,6 @@
 class Instrument < ActiveRecord::Base
-  belongs_to :underlying
+  belongs_to :underlying, class_name: 'Instrument', foreign_key: :underlying_id
   belongs_to :exchange
+
+  has_many :derivaties
 end
