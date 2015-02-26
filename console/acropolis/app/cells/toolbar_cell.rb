be924ed458429fa4d47594803249d882cb42256b
babaru <babaru.trinit@gmail.com>
2015-02-26 17:04:58 +0800

    change more page header to cell implementation

--- console/acropolis/app/cells/toolbar_cell.rb
+++ console/acropolis/app/cells/toolbar_cell.rb
@@ -1,7 +1,10 @@
 class ToolbarCell < Cell::Rails
 
   def show(args)
+    @css_class = ['btn-toolbar']
     @toolbar = args[:data]
+    @css_class << 'pull-right' if @toolbar[:align] == :right
+    @css_class = [@css_class, @toolbar[:css_class]].flatten
     render
   end
 
